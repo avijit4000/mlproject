8e0ce745e79604c0a576074a87a1ce4c11e44e53
avijit <avijit4000@gmail.com>
2023-08-13 09:10:17 +0530

    pipeline implementation

--- src/pipeline/prediction_pipeline.py
+++ src/pipeline/prediction_pipeline.py
@@ -1,9 +1,7 @@
 import sys
-import os
-from src.exception import CustomException
-from src.logger import logging
-from src.utils import load_object
 import pandas as pd
+from src.exception import CustomException
+from src.utils import load_object
 
 
 class PredictPipeline:
@@ -12,60 +10,59 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
+            model_path=os.path.join("artifacts","model.pkl")
             preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
-            model_path=os.path.join('artifacts','model.pkl')
-
-            preprocessor=load_object(preprocessor_path)
-            model=load_object(model_path)
-
+            print("Before Loading")
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            print("After Loading")
             data_scaled=preprocessor.transform(features)
-
-            pred=model.predict(data_scaled)
-            return pred
-            
-
+            preds=model.predict(data_scaled)
+            return preds
+        
         except Exception as e:
-            logging.info("Exception occured in prediction")
             raise CustomException(e,sys)
-        
-class CustomData:
-    def __init__(self,
-                 carat:float,
-                 depth:float,
-                 table:float,
-                 x:float,
-                 y:float,
-                 z:float,
-                 cut:str,
-                 color:str,
-                 clarity:str):
-        
-        self.carat=carat
-        self.depth=depth
-        self.table=table
-        self.x=x
-        self.y=y
-        self.z=z
-        self.cut = cut
-        self.color = color
-        self.clarity = clarity
 
-    def get_data_as_dataframe(self):
+
+
+class CustomData:
+    def __init__(  self,
+        gender: str,
+        race_ethnicity: str,
+        parental_level_of_education,
+        lunch: str,
+        test_preparation_course: str,
+        reading_score: int,
+        writing_score: int):
+
+        self.gender = gender
+
+        self.race_ethnicity = race_ethnicity
+
+        self.parental_level_of_education = parental_level_of_education
+
+        self.lunch = lunch
+
+        self.test_preparation_course = test_preparation_course
+
+        self.reading_score = reading_score
+
+        self.writing_score = writing_score
+
+    def get_data_as_data_frame(self):
         try:
             custom_data_input_dict = {
-                'carat':[self.carat],
-                'depth':[self.depth],
-                'table':[self.table],
-                'x':[self.x],
-                'y':[self.y],
-                'z':[self.z],
-                'cut':[self.cut],
-                'color':[self.color],
-                'clarity':[self.clarity]
+                "gender": [self.gender],
+                "race_ethnicity": [self.race_ethnicity],
+                "parental_level_of_education": [self.parental_level_of_education],
+                "lunch": [self.lunch],
+                "test_preparation_course": [self.test_preparation_course],
+                "reading_score": [self.reading_score],
+                "writing_score": [self.writing_score],
             }
-            df = pd.DataFrame(custom_data_input_dict)
-            logging.info('Dataframe Gathered')
-            return df
+
+            return pd.DataFrame(custom_data_input_dict)
+
         except Exception as e:
-            logging.info('Exception Occured in prediction pipeline')
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)
+
